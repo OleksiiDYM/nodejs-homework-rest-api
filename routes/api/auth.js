3e0b32603ce9e0ba66ea198404e733144cac2d4b
Oleksii <al.dimitrash@gmail.com>
2023-10-09 20:34:23 +0300

    update

---
+++ routes/api/auth.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const {
+  validateBody,
+  authenticate,
+  upload,
+  avatarOptimizer,
+} = require("../../middlewares");
+const { schemas } = require("../../models/user");
+const ctrl = require("../../controllers/users");
+
+const router = express.Router();
+
+router.post("/register", validateBody(schemas.registerSchema), ctrl.register);
+router.get("/verify/:verificationToken", ctrl.verifyEmail);
+router.post(
+  "/verify",
+  validateBody(schemas.emailSchema),
+  ctrl.resendVerifyEmail
+);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.get("/current", authenticate, ctrl.getCurrent);
+router.post("/logout", authenticate, ctrl.logout);
+router.patch("/", authenticate, ctrl.updateSubscription);
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  avatarOptimizer,
+  ctrl.updateAvatar
+);
+
+module.exports = router;
